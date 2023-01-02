3264b3f4db3b3a2d8316bfeb8f6896b3eced1c05
Muhammad Rizwan <f190994@nu.edu.pk>
2023-01-03 00:29:13 +0500

    project files

---
+++ app/src/main/java/com/example/scrapproject/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.scrapproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+/*
+{
+   "username":"dr.fiaz",
+    "password":"123456",
+    "contact":"03497668384",
+    "address":"sargodha",
+    "email":"faiz123@gmail.com"
+
+
+}
+ */
