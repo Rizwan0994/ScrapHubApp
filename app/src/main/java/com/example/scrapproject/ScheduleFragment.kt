b63de858a118e6b9af04c076c8a8a75000048990
Muhammad Rizwan <f190994@nu.edu.pk>
2023-02-24 03:22:00 +0500

    set time and date selector

--- app/src/main/java/com/example/scrapproject/ScheduleFragment.kt
+++ app/src/main/java/com/example/scrapproject/ScheduleFragment.kt
@@ -1,6 +1,9 @@
 package com.example.scrapproject
 
 import android.R
+import android.app.AlertDialog
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -20,6 +24,9 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,7 +35,7 @@ class ScheduleFragment : Fragment() {
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
     private val scheduleViewModel by viewModels<ScheduleViewModel>()
-    private val schedulePickup:ScheduleResponse? = null
+    private val schedulePickup: ScheduleResponse? = null
 
     @Inject
     lateinit var scheduleAPI: ScheduleAPI
@@ -43,8 +50,8 @@ class ScheduleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val items= arrayOf("FaisalabadSH1","FaisalabadSH2","FaisalabadSH3")
-        val adapter= ArrayAdapter(view.context, R.layout.simple_list_item_1, items)
+        val items = arrayOf("FaisalabadSH1", "FaisalabadSH2", "FaisalabadSH3")
+        val adapter = ArrayAdapter(view.context, R.layout.simple_list_item_1, items)
         binding.EdittxtSelectNearestYard.setAdapter(adapter)
         bindHandlers()
         bindObservers()
@@ -52,7 +59,7 @@ class ScheduleFragment : Fragment() {
 
     private fun bindObservers() {
         scheduleViewModel.statusLiveData.observe(viewLifecycleOwner, Observer {
-            when(it) {
+            when (it) {
                 is NetworkResult.Success -> {
                     findNavController().popBackStack()
                 }
@@ -66,29 +73,86 @@ class ScheduleFragment : Fragment() {
         })
     }
 
-    private fun bindHandlers(){
+    private fun bindHandlers() {
 
         binding.apply {
-        binding.btnProceed.setOnClickListener{
-        val nearestYard= EdittxtSelectNearestYard.text.toString()
-        val sDate=txtSDate.text.toString()
-        val sTime=txtSTime.text.toString()
-        val scrapItem=txtScrapItemDetails.text.toString()
-        val scheduleRequest=ScheduleRequest(nearestYard,sDate,sTime,scrapItem)
-        if(schedulePickup==null){
-            scheduleViewModel.createSchedule(scheduleRequest)
+
+            //timepicker..........
+            var selectedTime = ""
+            binding.btnTimeSelected.setOnClickListener {
+                // Get the current hour and minute values
+                val calendar = Calendar.getInstance()
+                val hour = calendar.get(Calendar.HOUR_OF_DAY)
+                val minute = calendar.get(Calendar.MINUTE)
+
+                // Create a timeSetListener to handle the user's selection of a time
+                val timeSetListener =
+                    TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                        // Do something with the selected hour and minute values
+                        selectedTime = String.format("%02d:%02d", hourOfDay, minute)
+
+                    }
+                val context: Context = binding.root.context
+                TimePickerDialog(context, timeSetListener, hour, minute, true).show()
+
+            }
+            //end time picker...
+            binding.btnProceed.setOnClickListener {
+                val nearestYard = EdittxtSelectNearestYard.text.toString()
+                //date picker set
+                val year = datePicker1.year
+                val month = datePicker1.month
+                val dayOfMonth = datePicker1.dayOfMonth
+
+                val selectedDate = LocalDate.of(year, month + 1, dayOfMonth)
+                val formattedDate = selectedDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+                val sDate = selectedDate.toString()
+
+
+
+                //val sTime=txtSTime.text.toString()
+                    val scrapItem = txtScrapItemDetails.text.toString()
+
+                //dialog box...
+                val builder = AlertDialog.Builder(context)
+                if (nearestYard?.isNotEmpty() && scrapItem?.isNotEmpty() && formattedDate?.isNotEmpty() == true && selectedTime?.isNotEmpty()) {
+
+                    builder.setTitle("Confirmation")
+                    builder.setMessage("Are you sure you want to Schedule Pickup?")
+                    builder.setPositiveButton("Yes") { dialog, which ->
+                        // user clicked Yes
+                        val scheduleRequest = ScheduleRequest(
+                            nearestYard,
+                            scrapItem,
+                            formattedDate,
+                            selectedTime
+                        )
+                        if (schedulePickup == null) {
+                            scheduleViewModel.createSchedule(scheduleRequest)
+                            Toast.makeText(context, "Pickup Scheduled!", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                    builder.setNegativeButton("No") { dialog, which ->
+                        // user clicked No
+                        Toast.makeText(context, "Pickup Not Scheduled!", Toast.LENGTH_SHORT).show()
+                    }
+                    builder.show()
+
+                }else {
+                    Toast.makeText(context, "Some thing went wrong!", Toast.LENGTH_SHORT).show()
+                }
+                }
+
+
+            }
+
         }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
         }
-
-
-        }
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 
 
 }
